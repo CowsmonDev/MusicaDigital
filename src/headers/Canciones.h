58cda4ca47caad77d22a9f6e991adb77095c706f
SavanDev <DylanPhoenix2030@outlook.com.ar>
2021-10-20 02:29:34 -0300

    Rejunte
    - Proyecto base del profesor.
    - TDA's incompletos.
    (Necesitaré subir por separado esto más adelante)

--- src/headers/Canciones.h
+++ src/headers/Canciones.h
@@ -1,18 +1,24 @@
 #include <string>
+
 using namespace std;
+
 class Canciones{
 	public:
-		Canciones(const string & direccion) const;
-	private: 
-		struct cancion {
+		Canciones(const string & direccion);
+		Lista<string> obtenerTopCancionesGenero(string genero) const;
+		Lista<string> busqueda(string texto) const;
+		Lista<string> filtrado(int fInicial, int fFinal) const;
+	private:
+		void agregarElemento(const string & elemento);
+		struct Cancion {
 			string id;
 			string nombre;
 			string interprete;
 			string lanzamiento;
 			string duracion;
 			string genero;
-			unsigned int repoducciones;
+			unsigned int reproducciones;
 		};
 
-		Lista<cancion> * listado;
+		Lista<Cancion> * listado;
 };
