4e35166221ca42ccb7a7b891cfa638fb551cf2bd
CowsmonDev <agustin.crespo22@gmail.com>
2021-10-23 21:31:22 -0300

    Modificacion de Lista

--- src/Lista.cpp
+++ src/Lista.cpp
@@ -4,32 +4,23 @@ using namespace std;
 
 template <typename T>
 Lista<T>::Lista(){
-	this->primero = new Nodo();
-	this->primero->elemento = NULL;
+	this->siguiente = NULL;
 }
 
 template <typename T>
 void Lista<T>::agregarElemento(T elemento){
-	Nodo * nuevoElemento = new Nodo();
-	nuevoElemento->elemento = elemento;
-	nuevoElemento->siguiente = this->primero;
-	this->primero = nuevoElemento;
+	Lista<T> * nuevaLista = new Lista<T>();
+	nuevaLista = this;
+	this->elemento = elemento;
+	this->siguiente = nuevaLista;
 }
 
 template<typename T>
 void Lista<T>::eliminarElemento(int indice){
-	Nodo * cursor = primero;
-	int i = 0;
-	while ((i != indice) && (cursor->elemento != NULL)){
-		cursor = cursor->siguiente;
-		i++;
-	}
-	if (cursor->elemento != NULL)
-	{
-		Nodo * eliminado = cursor;
-		cursor = cursor->siguiente;
-		delete eliminado;
-	}
+	Lista<T> * eliminado = this;
+	this->elemento = this->siguiente->elemento;
+	this->siguiente = this->siguiente->siguiente;
+	delete eliminado;
 }
 
 template<typename T>
@@ -39,10 +30,29 @@ void Lista<T>::eliminarPrimero(){
 
 template<typename T>
 T Lista<T>::obtenerElemento() const{
-	return primero->elemento;
+	return elemento;
 }
 
 template<typename T>
 Lista<T> * Lista<T>::obtenerSiguiente() const{
+<<<<<<< Updated upstream
 	return primero->siguiente;
 }
+=======
+	return siguiente;
+}
+
+template<typename T>
+void Lista<T>::operator=(Lista<T> nuevaLista){
+	this->siguiente = nuevaLista->siguiente;
+	this->elemento = nuevaLista->elemento;
+}
+
+template<typename T>
+bool Lista<T>::listaVacia() const{
+	return (this->siguiente == NULL);
+}
+
+template class Lista<Canciones::cancion>;
+template class Lista<string>;
+>>>>>>> Stashed changes
--- src/headers/Lista.h
+++ src/headers/Lista.h
@@ -1,3 +1,5 @@
+#ifndef Lista_T
+#define Lista_T
 template <typename T>
 
 class Lista{
@@ -8,10 +10,20 @@ class Lista{
 		void eliminarElemento(int indice);
 		T obtenerElemento() const;
 		Lista<T> * obtenerSiguiente() const;
+		void operator=(Lista<T> nuevaLista);
+		bool listaVacia() const;
 	private:
+<<<<<<< Updated upstream
 		struct Nodo{
 			T elemento;
 			Lista<T> * siguiente;
 		}
 		Nodo * primero;
 };
+=======
+		T elemento;
+		Lista<T> * siguiente;
+};
+
+#endif
+>>>>>>> Stashed changes
